f193324d32a64924c492bdea1a6bcf4a7da1c8d6
Cristian Racedo <cristiandracedo@hotmail.com>
2022-09-12 02:37:35 -0300

    Actualizacion script de contacto

--- scripts/contact.php
+++ scripts/contact.php
@@ -2,18 +2,14 @@
 	$nombre   = $_POST['nombre'];
 	$email 	  = $_POST['email'];
 	$tipo 	  = $_POST['tipo'];	
-	$consulta = $_POST['consulta'];
-	$mensaje  = "
-		<strong> Nombre: </strong> $nombre <br/>
-		<strong> Correo: </strong> $email <br/>
-		<strong> Consulta: </strong> $consulta";	
+	$consulta = $_POST['consulta'];	
 	mail(
 		"CristianDRacedo@hotmail.com",
-		$tipo,	
-		$mensaje,
+		$tipo,
+		$consulta,
 		"From: $nombre <$email>
 		\n Reply-To: $email
 		\n Content-Type: text/html;
 		\n charset=utf-8"
 	);
-	header( "location: https://c215714n.github.io/EducacionIT" );
+	header( "location: ../" );
